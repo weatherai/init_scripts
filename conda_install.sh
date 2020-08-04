117bdbacb481556484da80994580a75d3edda18b
Marvin <m.gabler@qmet.de>
2020-08-04 13:18:52 +0200

    init

---
+++ conda_install.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install curl -y
+curl -O https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
+sh Anaconda3-2020.07-Linux-x86_64.sh
---
+++ gdal_cdo_pymongo_stack.sh
@@ -0,0 +1,9 @@
+conda install -c conda-forge gdal -y
+conda install -c anaconda numpy -y
+conda install -c anaconda pandas -y
+conda install -c anaconda pymongo -y
+conda install -c conda-forge cdo -y
+conda install -c conda-forge fastapi -y
+conda install -c conda-forge uvicorn -y
+pip3 install slowapi -y
+
